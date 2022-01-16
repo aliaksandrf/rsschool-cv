c3e5d27f57941e4f783e88970dc5740a3f1199bf
Aliaksandr Fedukovich <1632637@mail.ru>
2022-01-16 21:30:43 +0300

    feat: add adaptive footer

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,7 @@
+document.getElementById('hamburger-1').click = function() {
+    document.getElementById("hamburger-1").classList.toggle("is-active");
+}
+
+
+
 console.log("Вёрстка валидная +10 \n Вёрстка семантическая +20 \n Вёрстка соответствует макету +48 \n Требования к css + 12 \n Интерактивность, реализуемая через css +20 \n ");
