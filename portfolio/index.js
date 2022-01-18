c963b3185d7c00b28735be2bb4f10d9e973067a5
Aliaksandr Fedukovich <1632637@mail.ru>
2022-01-18 16:23:53 +0300

    feat: update header for adaptive

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,13 @@
-document.getElementById('hamburger-1').click = function() {
-    document.getElementById("hamburger-1").classList.toggle("is-active");
+const listener = document.querySelector(".adaptive__menu");
+const sidemenu = document.querySelector('.side-menu')
+if(listener) {
+    listener.addEventListener('click', toggleMenu);
 }
 
+function toggleMenu() {
+    listener.classList.toggle("open");
+    sidemenu.classList.toggle('close');
+}
 
 
 console.log("Вёрстка валидная +10 \n Вёрстка семантическая +20 \n Вёрстка соответствует макету +48 \n Требования к css + 12 \n Интерактивность, реализуемая через css +20 \n ");
